825adf64a7f33f2c7ce2cfffa932959ffbfcf017
AlexV <versh2330@gmail.com>
2017-03-21 11:12:32 +0300

    Update ResultsPage.java

--- src/test/java/testclass/pages/ResultsPage.java
+++ src/test/java/testclass/pages/ResultsPage.java
@@ -20,7 +20,7 @@ import static org.testng.AssertJUnit.assertTrue;
  * Created by Alexandr.Vershok on 3/20/2017.
  */
 public class ResultsPage extends BaseForm {
-
+//TODO private String[] linksArray; copy links from List listOfResults
     private Label company = new Label(By.xpath(".//*[@class=\"i-checkbox__real\" and @value=\"samsung\"][1]/following-sibling::span"), "samsung");
     private TextBox priceFilter = new TextBox(By.xpath(".//*[@class=\"schema-filter__group\"]/div[2]/input[@placeholder=\"до\"]"),"Price less than field");
     private TextBox producedAfter = new TextBox(By.xpath(".//*[@class=\"schema-filter__group\"]/div[1]/input[@placeholder=\"2010\"]"),"Produced After");
@@ -46,17 +46,19 @@ public class ResultsPage extends BaseForm {
     }
 
 
-
+//@Parameters({"Year", "Make", "Size", "Price")
     public void checkResults(){
         String url = Browser.getLocation();
         System.out.println("Starting step4");
         listOfResults = Browser.getDriver().findElements(By.xpath(".//*[@class=\"schema-product__group\"]/div/div/div[2]/a/img"));
-
-        for(WebElement result : listOfResults){ //stale element reference
+        //linkArray = new String[listOfResults.size()];?
+        for(WebElement result : listOfResults){ //stale element reference; replace by linkArray
 
             result.click();
             System.out.println("Searching for year");
             String year = new Label(By.xpath(".//*[@class=\"product-specs__table\"]/tbody[1]/tr[2]/td[2]/span[contains(.,\"2013\") or contains(.,\"2014\") or contains(.,\"2015\")  or contains(.,\"2016\")  or contains(.,\"2017\")]"),"Check Year").getText();
+           //split(" ") the year. get the year[0], 
+            // check @param Year is <= Integer.parseInt(year[0])
             String price = new Label(By.xpath(".//*[@class=\"offers-description__flex\"]/div/div[1]/a"),"Check price").getText();
             String lowestPrice = price.split(",")[0];
             int lPrice = Integer.parseInt(lowestPrice);
